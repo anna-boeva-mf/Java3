eb5fb17d7db6e7313f72f5a0ce39055f7bce253b
anna-boeva-mf <anna.boeva16@gmail.com>
2021-06-16 15:53:02 +0300

    Добавлено ДЗ5

--- src/main/java/lesson5/Car.java
+++ src/main/java/lesson5/Car.java
@@ -1,33 +1,54 @@
 package lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
+
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            MainClass.countDownLatch.countDown();
             System.out.println(this.name + " готов");
+            MainClass.cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        try {
+            if (MainClass.lock.tryLock()) {
+                System.out.println(this.name + " выиграл гонку");
+                MainClass.countDownLatchEnd.countDown();
+                MainClass.cyclicBarrier.await();
+                MainClass.lock.unlock();
+            } else {
+                MainClass.countDownLatchEnd.countDown();
+                MainClass.cyclicBarrier.await();
+            }
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/lesson5/Tunnel.java
+++ src/main/java/lesson5/Tunnel.java
@@ -1,5 +1,7 @@
 package lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
     public Tunnel() {
         this.length = 80;
@@ -7,15 +9,19 @@ public class Tunnel extends Stage {
     }
     @Override
     public void go(Car c) {
+        int maxCars = MainClass.CARS_COUNT/2;
+        Semaphore smp = new Semaphore(maxCars);
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
